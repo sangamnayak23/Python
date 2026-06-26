a2e2edcf5520e806e4a7f2a41bc355c809fe1694
sangamnayak23 <sangamnayak23@gmail.com>
2026-06-27 00:33:50 +0530

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,20 @@
+# Simple Calculator using python programming language 
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+operation = input("Enter operation (+, -, *, /): ")
+
+if operation == "+":
+    print("Result:", num1 + num2)
+elif operation == "-":
+    print("Result:", num1 - num2)
+elif operation == "*":
+    print("Result:", num1 * num2)
+elif operation == "/":
+    if num2 != 0:
+        print("Result:", num1 / num2)
+    else:
+        print("Error: Division by zero is not allowed.")
+else:
+    print("Invalid operation.")
