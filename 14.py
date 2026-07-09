d992bc451284a6e9947193cd697ca9dee0ad5fff
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-09 10:40:01 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,10 @@
+# Even or Odd Checker using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check whether the number is even or odd
+if number % 2 == 0:
+    print("The number is Even.")
+else:
+    print("The number is Odd.")
