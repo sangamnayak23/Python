810a8cb1c3a04129e762035a1dc824f92c37bbf0
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-14 06:21:03 +0530

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,10 @@
+# Reverse a String using python programming language 
+
+# Get a string from the user
+text = input("Enter a string: ")
+
+# Reverse the string
+reversed_text = text[::-1]
+
+# Display the result
+print("Reversed string:", reversed_text)
