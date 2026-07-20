e20d674b4f1c5f217b2de8c291243c5bd0aef10c
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-20 08:39:58 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,11 @@
+# Find the Smallest Number using python programming language 
+
+# Create a list of numbers
+numbers = [25, 10, 45, 5, 18]
+
+# Find the smallest number
+smallest = min(numbers)
+
+# Display the result
+print("List:", numbers)
+print("Smallest number is:", smallest)
