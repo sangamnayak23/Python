c95704d0de6e1bb35f2faeed698549a047d0e448
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-23 14:33:12 +0530

    Create 26.py

---
+++ 26.py
@@ -0,0 +1,10 @@
+# String Length Finder using python programming language 
+
+# Get a string from the user
+text = input("Enter a string: ")
+
+# Find the length of the string
+length = len(text)
+
+# Display the result
+print("Length of the string is:", length)
