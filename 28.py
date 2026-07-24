53e68c1b6d8c48bf4a4c840a246b456121f4b6bd
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-25 00:53:12 +0530

    Create 28.py

---
+++ 28.py
@@ -0,0 +1,12 @@
+# Positive, Negative, or Zero Checker using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check the number
+if number > 0:
+    print("The number is Positive.")
+elif number < 0:
+    print("The number is Negative.")
+else:
+    print("The number is Zero.")
