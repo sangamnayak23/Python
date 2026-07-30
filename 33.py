aaa3c1104e7f8a92cf2874cb0a050cdc8fa99457
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-30 16:05:30 +0530

    Create 33.py

---
+++ 33.py
@@ -0,0 +1,10 @@
+# Square of a Number using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Calculate the square
+square = number * number
+
+# Display the result
+print("Square of", number, "is", square)
