0d67a4714cba23113d37de640e43954537309bf0
sangamnayak23 <sangamnayak23@gmail.com>
2026-07-31 15:24:23 +0530

    Create 34.py

---
+++ 34.py
@@ -0,0 +1,12 @@
+# Average of Three Numbers using python programming language 
+
+# Get three numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Calculate the average
+average = (num1 + num2 + num3) / 3
+
+# Display the result
+print("Average =", average)
