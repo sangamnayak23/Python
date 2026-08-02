27e51b4a2d433f4de1a9b0c01f810310b43ea397
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-02 23:12:30 +0530

    Create 36.py

---
+++ 36.py
@@ -0,0 +1,10 @@
+# Character Counter using python programming language 
+
+# Get a string from the user
+text = input("Enter a string: ")
+
+# Count the characters
+count = len(text)
+
+# Display the result
+print("Total characters:", count)
