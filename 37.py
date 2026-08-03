5fcae91466b29a35c3eb0438441967c70b4ff801
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-03 11:12:14 +0530

    Create 37.py

---
+++ 37.py
@@ -0,0 +1,10 @@
+# Divisibility Checker using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is divisible by 5
+if number % 5 == 0:
+    print(number, "is divisible by 5.")
+else:
+    print(number, "is not divisible by 5.")
