9b07e411c857b5797955be1d9d47b76695ee57d8
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-04 09:42:03 +0530

    Create 38.py

---
+++ 38.py
@@ -0,0 +1,10 @@
+# Cube of a Number using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Calculate the cube
+cube = number ** 3
+
+# Display the result
+print("Cube of", number, "is", cube)
