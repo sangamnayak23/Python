f867ddfa6a1b7903955dddf16a324b506b35c050
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-06 00:02:54 +0530

    Create 39.py

---
+++ 39.py
@@ -0,0 +1,10 @@
+# Positive Number Checker using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check whether the number is positive
+if number > 0:
+    print(number, "is a Positive Number.")
+else:
+    print(number, "is not a Positive Number.")
