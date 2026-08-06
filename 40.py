7b4cc77deafe711baea4ecdafb454290b0ac4bf8
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-07 00:11:00 +0530

    Create 40.py

---
+++ 40.py
@@ -0,0 +1,10 @@
+# Multiple of 10 Checker using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is a multiple of 10
+if number % 10 == 0:
+    print(number, "is a multiple of 10.")
+else:
+    print(number, "is not a multiple of 10.")
