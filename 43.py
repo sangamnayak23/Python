a767f4f42811b88edc58d22c2a735878dca350e5
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-10 00:25:51 +0530

    Create 43.py

---
+++ 43.py
@@ -0,0 +1,10 @@
+# Area of a Circle using python programming language 
+
+# Get the radius from the user
+radius = float(input("Enter the radius: "))
+
+# Calculate the area
+area = 3.14 * radius * radius
+
+# Display the result
+print("Area of the circle:", area)
