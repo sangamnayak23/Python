26dda71f5a77f0895d07983603f7b9552c08d50c
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-13 00:27:23 +0530

    Create 46.py

---
+++ 46.py
@@ -0,0 +1,10 @@
+# Perimeter of a Square using python programming language 
+
+# Get the side length from the user
+side = float(input("Enter the side of the square: "))
+
+# Calculate the perimeter
+perimeter = 4 * side
+
+# Display the result
+print("Perimeter of the square:", perimeter)
