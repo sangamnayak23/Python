c91a299abdb2f3d007155fbf4045ffc6611ef674
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-15 00:05:48 +0530

    Create 48.py

---
+++ 48.py
@@ -0,0 +1,10 @@
+# Fahrenheit to Celsius Converter using python programming language 
+
+# Get temperature in Fahrenheit
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+
+# Convert Fahrenheit to Celsius
+celsius = (fahrenheit - 32) * 5 / 9
+
+# Display the result
+print("Temperature in Celsius:", celsius)
