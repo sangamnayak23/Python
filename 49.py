56cedc004a042ef255a8f494e68d4ae3108e474d
sangamnayak23 <sangamnayak23@gmail.com>
2026-08-16 00:03:16 +0530

    Create 49.py

---
+++ 49.py
@@ -0,0 +1,13 @@
+# Word Counter using python programming language 
+
+# Get a sentence from the user
+sentence = input("Enter a sentence: ")
+
+# Split the sentence into words
+words = sentence.split()
+
+# Count the words
+count = len(words)
+
+# Display the result
+print("Number of words:", count)
