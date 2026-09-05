da1f066afc4e04cbc9c8e758ea6cddd01911fa96
sangamnayak23 <sangamnayak23@gmail.com>
2026-09-05 23:52:07 +0530

    Create 66.py

---
+++ 66.py
@@ -0,0 +1,10 @@
+# Count Digits in a Number using python programming language 
+
+# Get a number from the user
+number = input("Enter a number: ")
+
+# Count the digits
+count = len(number)
+
+# Display the result
+print("Number of digits:", count)
