ca5a6470f088b78cf22d667334352404e16f3c4a
sangamnayak23 <sangamnayak23@gmail.com>
2026-09-10 21:08:58 +0530

    Create 69.py

---
+++ 69.py
@@ -0,0 +1,7 @@
+# First Character Finder using python programming language 
+
+# Get a string from the user
+text = input("Enter a word: ")
+
+# Display the first character
+print("First character:", text[0])
