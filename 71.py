85a356a16fa1e86564f12b25b876bb9759f38fa2
sangamnayak23 <sangamnayak23@gmail.com>
2026-09-12 22:10:17 +0530

    Create 71.py

---
+++ 71.py
@@ -0,0 +1,12 @@
+# Positive, Negative or Zero using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check the number
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
