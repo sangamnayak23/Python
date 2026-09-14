a9e1f95c8861b43090b039bf65876cc1ebab075b
sangamnayak23 <sangamnayak23@gmail.com>
2026-09-15 00:01:58 +0530

    Create 73.py

---
+++ 73.py
@@ -0,0 +1,10 @@
+# Divisibility by 3 Checker using python programming language 
+
+# Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is divisible by 3
+if number % 3 == 0:
+    print("The number is divisible by 3.")
+else:
+    print("The number is not divisible by 3.")
