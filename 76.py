5384ff0f54269f83579887afed132fa52759fcc9
sangamnayak23 <sangamnayak23@gmail.com>
2026-09-20 23:00:23 +0530

    Create 76.py

---
+++ 76.py
@@ -0,0 +1,11 @@
+# Sum of Even Numbers using python programming language 
+
+# Create a list of numbers
+numbers = [10, 15, 20, 25, 30]
+
+# Find the sum of even numbers
+total = sum(num for num in numbers if num % 2 == 0)
+
+# Display the result
+print("Numbers:", numbers)
+print("Sum of even numbers:", total)
